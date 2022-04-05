ce2d5d79d6b5ac9ffb88545c7ca5d5da41556df3
Th35kyB0y <mishradeepak073@gmail.com>
2022-04-05 18:09:15 +0530

    ss

--- server.js
+++ server.js
@@ -9,9 +9,11 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, './client/build')))
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/client/build'))
+    
 })
 app.get("/fetch",(req,res)=>{
-  res.send("connected")
+  res.send("new")
+  
 })
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
