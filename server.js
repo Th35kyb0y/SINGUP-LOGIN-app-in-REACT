2be32838b8b1045b36403512ba00992d91754cb7
Th35kyb0y <38756178+Th35kyb0y@users.noreply.github.com>
2022-04-06 01:24:44 +0530

    Update server.js
    n

--- server.js
+++ server.js
@@ -6,15 +6,10 @@ const express = require('express');
 const path = require("path");
 
 const app = express();
-app.use(cookieParse())
 // port number , env for heroku
 let port = 5000||process.env.PORT;
 // serve static files from frontend build/static , ( for deploying in heroku)
 app.use( express.static(path.join(__dirname,'/client/build')))
-// whatever the url , /url , it will redirect to index.html of reactjs
-app.get("/*",(req,res)=>{
-  res.sendFile(path.join(__dirname,"client/build","index.html"))
-}) 
 
 
 
